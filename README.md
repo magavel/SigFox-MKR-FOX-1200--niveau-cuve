371b249c7bc15fc6ae96bb6f567ac61f5bea928e
magavel <avel.feron@gmail.com>
2019-02-12 19:40:49 +0100

    crea fichier sonde.ino

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Attention projet en cours de développement==> non fonctionnel a ce jour
 
 **Besoin**:
 - Utilisation d'un module arduino MKR FOX 1200 
-- Sonde étanche à ultrason
+- Sonde étanche à ultrason DYP-ME007Y ==> Module de Capteur à Ultrasons Gamme de Mesure 0.3m-3.5m
 
-**Bibliothèques nécessaire**
+**Bibliothèques nécessaires**
 
 
 ```
@@ -19,3 +19,17 @@ Mesure du niveau d'eau dans la cuve via la sond à ultrason.
 Envoie des données de mesures vers le backend de sigfox.
 Sigfox renvoie ces données via une callback vers le siteweb perso.
 Recuperation des données $_GET dans le fichier sigFox.php.
+
+
+
+
+
+
+
+
+## Sources et bibliographie:
+
+L'excellent site (breton oblige):
+http://karlytau.raidghost.com
+
+https://www.carnetdumaker.net/articles/mesurer-une-distance-avec-un-capteur-ultrason-hc-sr04-et-une-carte-arduino-genuino/
---
+++ src/sonde.ino
@@ -0,0 +1,95 @@
+
+
+
+#include <SigFox.h>
+#include <ArduinoLowPower.h>
+
+/* Constantes pour les broches */
+const byte TRIGGER_PIN = 2; // Broche TRIGGER
+const byte ECHO_PIN = 3;    // Broche ECHO
+ 
+/* Constantes pour le timeout */
+const unsigned long MEASURE_TIMEOUT = 25000UL; // 25ms = ~8m à 340m/s
+
+/* Vitesse du son dans l'air en mm/us */
+const float SOUND_SPEED = 340.0 / 1000;
+
+/** Fonction setup() */
+void setup() {
+   
+  /* Initialise le port série */
+  Serial.begin(9600);
+  //Serial.begin(115200);
+
+
+ if (!SigFox.begin()) {
+    Serial.println("pb avec sigfox!");
+    return;
+  }
+
+
+  /* Initialise les broches */
+  pinMode(TRIGGER_PIN, OUTPUT);
+  digitalWrite(TRIGGER_PIN, LOW); // La broche TRIGGER doit être à LOW au repos
+  pinMode(ECHO_PIN, INPUT);
+}
+ 
+/** Fonction loop() */
+void loop() {
+  
+  /* 1. Lance une mesure de distance en envoyant une impulsion HIGH de 10µs sur la broche TRIGGER */
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  
+  /* 2. Mesure le temps entre l'envoi de l'impulsion ultrasonique et son écho (si il existe) */
+  long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
+   
+  /* 3. Calcul la distance à partir du temps mesuré */
+  float distance_mm = measure / 2.0 * SOUND_SPEED;
+   
+  /* Affiche les résultats en mm, cm et m */
+  Serial.print(F("Distance: "));
+  Serial.print(distance_mm);
+  Serial.print(F("mm ("));
+  Serial.print(distance_mm / 10.0, 2);
+  Serial.print(F("cm, "));
+  Serial.print(distance_mm / 1000.0, 2);
+  Serial.println(F("m)"));
+   
+
+
+    String message;
+    message = distance_mm /10.0;
+
+
+    //envoie du message à la fonction sendString
+    sendString(message);
+  /* Délai d'attente 1 heure pour éviter d'afficher trop de résultats à la seconde */
+  delay(3600000);
+}
+
+
+void sendString(String str) {
+  // démarrage module sigfox
+  SigFox.begin();
+  // attente 100ms apres premiere configuration
+  delay(100);
+  // Clears all pending interrupts
+  SigFox.status();
+  delay(1);
+
+  SigFox.beginPacket();
+  SigFox.print(str);
+
+  int ret = SigFox.endPacket();  // send buffer to SIGFOX network
+  if (ret > 0) {
+    Serial.println("No transmission");
+  } else {
+    Serial.println("Transmission ok");
+  }
+
+  Serial.println(SigFox.status(SIGFOX));
+  Serial.println(SigFox.status(ATMEL));
+  SigFox.end();
+}
